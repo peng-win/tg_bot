0e059d5d12e7ee37d36b16fcb308cf13a315ff32
Ирина Гусева <isirasa2003@gmail.com>
2022-03-03 06:02:32 +0300

    extracting data from the database

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+using Npgsql;
 
 namespace Data.Models
 {
@@ -14,4 +16,19 @@ namespace Data.Models
         public string Patronymic { get; set; }
         public string Phone { get; set; }
     }
+    /*
+    public int InsertWithSql()
+    {
+        using (var conn = new NpgsqlConnection("Host=localhost;Username=postgres;Password=4815162342;Database=tgbotdb")) 
+        {
+            User user = new User();
+            user.FirstName = "Dapper01";
+            user.LastName = "Zhoukou";
+            user.Patronymic = "15";
+            user.Phone = "15";
+            // string _sql = "INSERT INTO User (имя, адрес, возраст) VALUES ('Dapper01', ' ', 13)";
+            string _sql = "INSERT INTO User(name,address,age)VALUES(@name,@address,@age)";
+            return conn.Execute(_sql, user);
+        }
+    }*/
 }
--- app/Services/TimedHostedService.cs
+++ app/Services/TimedHostedService.cs
@@ -3,6 +3,12 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineQueryResults;
+using Npgsql;
+
+using Dapper;
+
 namespace app.Services
 {
   
@@ -45,6 +51,7 @@ namespace app.Services
         
         async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
+            /*
             if (update.Type != Telegram.Bot.Types.Enums.UpdateType.Message)
                 return;
             if (update.Message!.Type != Telegram.Bot.Types.Enums.MessageType.Text)
@@ -54,11 +61,74 @@ namespace app.Services
             var messageText = update.Message.Text;
 
             Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+            /*
+            
 
             Message sentMessage = await botClient.SendTextMessageAsync(
                 chatId: chatId,
                 text: "Вы сказали: " + messageText,
-                cancellationToken: cancellationToken);
+              
+                cancellationToken: cancellationToken);*/
+
+            var chatId = update.Message.Chat.Id;
+            var messageText = update.Message.Text;
+            using (var connection = new NpgsqlConnection("Host=localhost;Username=postgres;Password=4815162342;Database=tgbotdb"))
+            {
+                //connection.Open();
+                //connection.Query<string>("Select first_name from UserTbl");
+                //connection.Execute("Insert into UserTbl (first_name, last_name, patronymic) values ('Иван','Иванов','Иванович');");
+                /*var value = connection.Query<string>("Select first_name from UserTbl;");
+                Console.WriteLine(value.First());*/
+
+                try
+                {
+
+                    connection.Open();
+                    NpgsqlCommand npgsqlCommand = connection.CreateCommand();
+                    npgsqlCommand.CommandText = "SELECT * FROM menutbl";
+                    NpgsqlDataReader npgsqlDataReader = npgsqlCommand.ExecuteReader();
+                    List<string[]> data = new List<string[]>();
+
+                    ReplyKeyboardMarkup keyboard = new(new[]
+                        {
+                            new KeyboardButton[] { "Меню" },
+                        })
+                    {
+                        ResizeKeyboard = true
+                    };
+                    /*InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+                    InlineKeyboardMarkup inlineKeyboard = new(new[]
+                        {
+                            new InlineKeyboardButton("Текст для первой кнопки","callback1"),
+                            new InlineKeyboardButton("Текст второй кнопки","callback2"),
+                        });
+                    )*/
+
+                    while (npgsqlDataReader.Read())
+                    {
+                        data.Add(new string[5]);
+                        data[data.Count - 1][0] = npgsqlDataReader[0].ToString();
+                        data[data.Count - 1][1] = npgsqlDataReader[1].ToString();
+                        data[data.Count - 1][2] = npgsqlDataReader[2].ToString();
+                        data[data.Count - 1][3] = npgsqlDataReader[3].ToString();
+                        data[data.Count - 1][4] = npgsqlDataReader[4].ToString();
+                    }
+                    npgsqlDataReader.Close();
+                    connection.Close();
+                    foreach (string[] s in data)
+                    {
+                        Message Message = await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: s[1].ToString() + " | " + s[2].ToString() + " | " + s[3].ToString(),
+                        replyMarkup: keyboard,
+                        cancellationToken: cancellationToken);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex);
+                }
+            };
         }
 
         Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
